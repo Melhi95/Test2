ded2b52b0456ccfe1e9fd937c2c4e58b27c12952
Melhi95 <melhi.92@mail.ru>
2024-10-26 19:46:56 +0300

    add  int x & y;

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,7 @@
 public class Main {
     public static void main(String[] args) {
+        int y = 1;
+        int x = 0;
         String str = "Hello world!";
         System.out.println(str);
     }
