03081a0038352b03a6b4a78355215d9a258ddbd6
BARDAK <melhi.92@mail.ru>
2024-10-26 19:55:43 +0300

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,6 @@
 public class Main {
     public static void main(String[] args) {
-        int y = 1;
+        int y = 1111;
         int x = 0;
         String str = "Hello world!";
         System.out.println(str);
