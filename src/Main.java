e268d7e2e45f8abbcd562f2d1ae3f21ff2125604
Melhi95 <melhi.92@mail.ru>
2024-10-27 06:35:34 +0300

    ver 0.1

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,4 @@
 import java.util.Scanner;
-
 public class Main {
     static final String[] arrayValidNumbers =  {"0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "10",
                                                 "I", "II", "III", "IV", "V", "VI", "VII", "VIII", "IX", "X",
